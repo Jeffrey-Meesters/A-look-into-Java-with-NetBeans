f1c687dfc741dcc785a13f407cdb268f66234d9a
jeffrey-meesters <jjeffreymeesters@gmail.com>
2016-12-22 16:04:25 +0100

    Build Interface and fish class

---
+++ src/hello/world/app/ViewFish.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello.world.app;
+
+/**
+ *
+ * @author jeffrey
+ */
+public interface ViewFish {
+    void swimSpeed(int increment);
+    
+    void eat(boolean newValue);
+}
